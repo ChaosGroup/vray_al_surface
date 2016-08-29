5a4aa6149ddde844c90c566532020458eb3e785d
Vladimir Koylazov <vlado@chaosgroup.com>
2016-08-29 12:36:17 +0300

    Added build_windows.bat file for building on Windows.

---
+++ std_plugin/build_windows.bat
@@ -0,0 +1,31 @@
+@echo off
+
+if not defined VRAY_SDK (
+	echo VRAY_SDK environment variable not defined.
+	goto done
+)
+
+if not defined VC_LIBS (
+	echo VC_LIBS environment variable not defined. It must match the lib folder of the V-Ray SDK i.e. vc101, vc11 etc
+	goto done
+)
+
+set VRAY_LIB_PATH=%VRAY_SDK%\lib\x64\%VC_LIBS%
+if not exist "%VRAY_LIB_PATH%" (
+	echo Folder "%VRAY_LIB_PATH%" does not exist; perhaps VC_LIBS variable has wrong value
+	goto done
+)
+
+rem Compiler options: full optimization, exception handling, dynamic multithreaded runtime
+set CPP_OPTIONS=/Ox /EHa /MD
+
+rem Libraries to link against
+set LIBS=vutils_s.lib plugman_s.lib vray.lib kernel32.lib user32.lib advapi32.lib
+
+rem Visual Studio compiler version
+set VC_LIBS=vc11
+
+cl vray_brdfalsurface.cpp ..\common\albrdf.cpp ..\common\beckmann.cpp ..\common\sss.cpp /I "%VRAY_SDK%\include" /I "%VRAY_SDK%\samples\vray_plugins\brdfs\vray_brdfbump" %CPP_OPTIONS% /link %LIBS% /libpath:"%VRAY_LIB_PATH%" /dll /out:".\bin\vray_BRDFAlSurface.dll"
+
+:done
+echo Done.
